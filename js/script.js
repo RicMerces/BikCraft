ea51a0bd7547d783f055ad13b539e1c8f91d053f
ricardoliveiraof2m <ricardoliveira0703@gmail.com>
2021-07-21 16:14:39 -0300

    Animação de slide adicionada a Portfolio + Modificações

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-new SimpleSlide({
-    slide: "quote", //Atributo data-slide="ThisIsBikcraft",
-    time: 5000
-});
+if (window.SimpleSlide){
+    new SimpleSlide({
+        slide: "quote", //Atributo data-slide="ThisIsBikcraft",
+        time: 5000
+    });
+    
+    
+    new SimpleSlide({
+        slide: "image", //Atributo data-slide="image",
+        time: 5000,
+        auto: true,
+        nav: true
+    });
+}   
